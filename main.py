a0675582c4035d1b358ad3d8e9f11af8dbb55d76
levbush <lev.o.bushuev@gmail.com>
2026-02-19 19:29:56 +0300

    temp

--- main.py
+++ main.py
@@ -17,15 +17,26 @@ class GameView(arcade.Window):
         self.manager = UIManager()
         self.anchor_layout = UIAnchorLayout()
         self.box_layout = UIBoxLayout()
-        self.button = UIFlatButton()
-        self.button.text = 'Change theme'
-        self.button.on_click = lambda _: self.change_theme()
-        self.box_layout.add(self.button)
+        
+        self.theme_button = UIFlatButton()
+        self.theme_button.text = 'Change theme'
+        self.theme_button.on_click = lambda _: self.change_theme()
+        self.theme_button.width = 250
+        
+        self.maptype_button = UIFlatButton()
+        self.maptype_button.text = 'Change map type'
+        self.maptype_button.on_click = lambda _: self.change_maptype()
+        self.maptype_button.width = 300
+        
+        self.box_layout.add(self.theme_button)
+        self.box_layout.add(self.maptype_button)
         self.anchor_layout.add(self.box_layout, anchor_y='bottom', align_y=50)
         self.manager.add(self.anchor_layout)
         self.manager.enable()
 
         self.theme_white = True
+        self.maptypes = ['map', 'driving', 'transit', 'admin']
+        self.curr_maptype = 0
 
     def setup(self):
         self.lon = input("Введите lon: ")
@@ -53,7 +64,7 @@ class GameView(arcade.Window):
         ll_spn = f'll={self.lon},{self.lat}&spn={self.spn[0]},{self.spn[1]}'
         # Готовим запрос.
 
-        map_request = f"{server_address}{ll_spn}&apikey={api_key}&theme={'light' if self.theme_white else 'dark'}"
+        map_request = f"{server_address}{ll_spn}&apikey={api_key}&theme={'light' if self.theme_white else 'dark'}&maptype={self.maptypes[self.curr_maptype]}"
         print(map_request)
         response = requests.get(map_request)
 
@@ -92,6 +103,10 @@ class GameView(arcade.Window):
 
     def change_theme(self):
         self.theme_white = not self.theme_white
+        self.get_image()
+
+    def change_maptype(self):
+        self.curr_maptype = (self.curr_maptype + 1) % len(self.maptypes)
         self.get_image()
 
 def main():
